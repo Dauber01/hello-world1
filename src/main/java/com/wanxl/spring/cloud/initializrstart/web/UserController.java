9ad6d9404a276f52b105e5c11eae2926ed455c79
Lucifer <88567982@q.com>
2018-07-10 16:13:08 +0800

    1

--- src/main/java/com/wanxl/spring/cloud/initializrstart/web/UserController.java
+++ src/main/java/com/wanxl/spring/cloud/initializrstart/web/UserController.java
@@ -2,12 +2,14 @@ package com.wanxl.spring.cloud.initializrstart.web;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.wanxl.spring.cloud.initializrstart.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Lucifer
@@ -18,9 +20,13 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private Map helloService;
+
     @GetMapping
     @JsonView(User.UserSimpleView.class)
     public List<User> queryAllUser(){
+        System.out.println(helloService);
         return Arrays.asList(new User(), new User(), new User());
     }
 
